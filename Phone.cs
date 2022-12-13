f04d5da06ca9c5047e2a909ba5a9ab9dd3201530
LiliyaBy <lilyabuhtik@gmail.com>
2022-12-14 02:03:17 +0300

    First commit

---
+++ Phone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_LiliyaBukhtik
+{
+    internal class Phone
+    {
+        public string number = "+79776221400";
+        public string model = "SamsungSM_N975F";
+        public string weight = "33";
+
+        public void ReceiveCall(string name)
+        {
+            Console.WriteLine("Calling {0}", name);
+        }
+
+
+        public string GetNumber()
+        { return number; }
+
+        public void ReceiveCall(string name, string number)
+        {
+            Console.WriteLine("Calling {0} number {1}", name, number);
+        }
+
+        public void SendMessage(params string[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+                Console.WriteLine($"Message sent {numbers[i]}");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lab5_LiliyaBukhtik
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Phone ex1 = new Phone();
+            Phone ex2 = new Phone();
+            Phone ex3 = new Phone();
+            Console.WriteLine(ex1.number);
+            Console.WriteLine(ex2.model);
+            Console.WriteLine(ex3.weight);
+            
+            ex1.ReceiveCall("Сat");
+            ex2.ReceiveCall("Gog");
+            ex3.ReceiveCall("SnoopDog");
+
+
+            Console.WriteLine(ex1.GetNumber());
+            Console.WriteLine(ex2.GetNumber());
+            Console.WriteLine(ex3.GetNumber());
+
+            ex1.ReceiveCall("Сat","+79776451321");
+            ex2.ReceiveCall("Gog", "+375339764354");
+            ex3.ReceiveCall("SnoopDog", "+12029861805");
+
+
+            ex1.SendMessage("+1202990764", "+375357864523");
+
+
+        }
+    }
+}
